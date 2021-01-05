2d9826495709be074fe79a6e21f55a79adcca1e2
chunga <clintonchunga@gmail.com>
2021-01-05 16:52:01 +0100

    i created my database in my workbench and connected to it sucessfully

--- index.js
+++ index.js
@@ -20,12 +20,12 @@ const promptMessages = {
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 3307,
 
     user: 'root',
 
     password: 'Prinsclint1.',
-    
+
     database: 'employees'
 });
 
